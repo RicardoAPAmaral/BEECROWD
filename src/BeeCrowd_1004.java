4f6906b4d05f503714b651ac8c9d792216548703
ricardo <2022210220004@iesp.edu.br>
2023-11-03 14:06:18 -0300

    BE_1004

---
+++ src/BeeCrowd_1004.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+
+public class BeeCrowd_1004 {
+    public static void main (String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        int prod=a*b;
+        System.out.println("PROD = "+prod);
+
+    }
+}
